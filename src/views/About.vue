ad4a52c484b7cd84ddd8b6716cad34de354a206a
Marco0198 <219049505@mycput.ac.za>
2022-01-09 06:37:39 +0200

    i added new navigation menu

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <Layout name="LayoutDefault">
+    <div><h1 class="text-center"></h1></div>
+  </Layout>
+</template>
+<script>
+import Layout from "@/layouts/Layout";
+
+export default {
+  components: { Layout },
+};
+</script>
---
+++ src/views/pages/Taskboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout name="LayoutDefault">
+    <div>
+       <create-task></create-task>
+       <display-all-task></display-all-task>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Layout from "@/layouts/Layout";
+import CreateTask from "@/components/task/CreateTask";
+import DisplayAllTask from "@/components/task/DisplayAllTask";
+export default {
+  components: {
+    Layout,CreateTask,DisplayAllTask
+  },
+  created() {
+    this.$store.dispatch("getCurrentUser");
+  },
+
+  computed: {
+    isLogin() {
+      return this.$store.getters["login/isLogin"];
+    },
+    user() {
+      return this.$store.getters.user;
+    },
+  },
+};
+</script>
