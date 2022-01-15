eab4c92bb3f42c2d4c672a2367622f42b1e3a4d4
Marco0198 <219049505@mycput.ac.za>
2022-01-15 15:09:14 +0200

    some changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -159,7 +159,7 @@ body {
   padding: 0;
   font-family: Arial;
   background: white;
-
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
 }
 
 .row-hero {
